caba9356990dc2ea9247556cbf0f41bc04f450ac
lk2745 <69129636+lk2745@users.noreply.github.com>
2020-09-20 20:59:14 -0400

    basic file setup

---
+++ webpack.prod.js
@@ -0,0 +1,83 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: { 
+                        limit: 8192, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    } 
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW({
+            // Do not precache images
+            exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+      
+            // Define runtime caching rules.
+            runtimeCaching: [{
+              // Match any request that ends with .png, .jpg, .jpeg or .svg.
+              urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      
+              // Apply a cache-first strategy.
+              handler: 'CacheFirst',
+      
+              options: {
+                // Use a custom cache name.
+                cacheName: 'images',
+      
+                // Only cache 10 images.
+                expiration: {
+                  maxEntries: 10,
+                },
+              },
+            }],
+          })
+    ]
+}
