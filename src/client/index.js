10939aec97e9ac234cd4fa9fecebeaa235a16ed5
lk2745 <69129636+lk2745@users.noreply.github.com>
2020-09-21 21:00:46 -0400

    basic files

---
+++ src/client/index.js
@@ -0,0 +1,26 @@
+//TODO Import statements
+
+/*import { handleSubmit } from './js/formHandler'
+import { validURL} from './js/urlValidation'*/
+
+import logo from './images/lklogo.jpg';
+import newsbackgroundImg  from './images/global-news.jpg';
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+// Assistance from https://medium.com/a-beginners-guide-for-webpack-2/handling-images-e1a2a2c28f8d 
+var logoImg = document.getElementById('logoImg');
+logoImg.src = logo;
+
+var mainImg = document.getElementById('mainImg');
+mainImg.src = newsbackgroundImg;
+
+//added export statements
+export {
+    handleSubmit,
+    validURL,
+}
